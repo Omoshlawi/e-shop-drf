3f0610361038cf38caff3faeb98289a808b579b6
Laurent Ouma <lawiomosh3@gmail.com>
2023-04-08 10:11:21 +0300

    using database data from online version

--- shop/models.py
+++ shop/models.py
@@ -57,8 +57,9 @@ class Product(models.Model):
         blank=True,
         null=True,
     )
-    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    additional_info = models.TextField(blank=True)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
